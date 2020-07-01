278a9b08999f64021ae07b68317b9e640b7d2dee
Ryan <santosryan131@gmail.com>
2020-07-01 12:41:07 -0300

    Exluded paths

--- src/middlewares/jwt.js
+++ src/middlewares/jwt.js
@@ -1,6 +1,19 @@
 const {verifyJwt} = require('../helpers/jwt');
 
 const checkJwt = (req, res, next) => {
+  // /auth/sign-in
+  // /auth/sign-up
+  // /auth/admin
+  // const adminsPaths = ['/auth/admin'];
+  const {url: path} = req;
+
+  const excludedPaths = ['/auth/sign-in','/auth/sign-up'];
+  const isExcluded = !!excludedPaths.find(p => p.startsWith(path));
+
+  console.log(path , isExcluded);
+  
+  if(isExcluded) return next();
+
   let token = req.headers['authorization'];
   token = token ? token.slice(7,token.length) : null;
   if(!token) {
